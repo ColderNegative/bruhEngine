afde919d7c2c1741a61a104b154688ecf151c1df
lquinnMac <lewis.quinn.22@ucl.ac.uk>
2024-02-22 17:59:59 +0000

    resource manager further developed

--- src/core/resource.cpp
+++ src/core/resource.cpp
@@ -1,1 +1,28 @@
 #include "resource.hpp"
+#include <functional>
+
+using namespace core;
+
+template <typename T>
+T* ResourceManager::get_resource(std::string* file_path) {
+    std::hash<std::string*> hash;
+    return m_resources[typeid(T)][hash(file_path)];
+}
+
+template <typename T>
+void ResourceManager::load_resource(Resource* resource) {
+    auto type_dict = m_resources[typeid(T)];
+    type_dict.insert_or_assign(resource->m_id, resource);
+}
+
+template <typename T>
+void ResourceManager::unload_resource(Resource* resource) {
+    auto type_dict = m_resources[typeid(T)];
+    type_dict.erase(resource->m_id);
+    delete resource;
+}
+
+Resource::Resource(std::string* file_path) : m_file_path(file_path) {
+    std::hash<std::string*> hash;
+    m_id = hash(file_path);
+}
--- src/core/resource.hpp
+++ src/core/resource.hpp
@@ -7,16 +7,19 @@
 namespace core {
 class Resource {
     public:
-        Resource();
+        std::string* m_file_path;
+        int m_id;
+
+        Resource(std::string* file_path);
         ~Resource();
 };
 
-class ResouceManager : public templates::Singleton<ResouceManager> {
+class ResourceManager : public templates::Singleton<ResourceManager> {
     public:
-        template <typename T> T* get_resource();
-        template <typename T> void load_resource(T* resource);
-        template <typename T> void unload_resource(T* resource);
+        template <typename T> T* get_resource(std::string* file_path);
+        template <typename T> void load_resource(Resource* resource);
+        template <typename T> void unload_resource(Resource* resource);
     private:
-        std::map<std::type_index, std::map<std::string, Resource*>> resources;
+        std::map<std::type_index, std::map<int, Resource*>> m_resources;
 };
 }
